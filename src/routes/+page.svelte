5e222e327535a0b67cbc4233a986185719837a28
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2024-06-06 18:31:41 +0300

    Added aspect ratio and dice count controls

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,20 @@
 	const size = 600;
 	let canvas: HTMLCanvasElement | undefined; // popuplated from `bind:this`
 
+	let originalImageWidth = $state(0);
+	let originalImageHeight = $state(0);
+	let lockOriginalImageAspectRatio = $state(true);
+	let originalImageAspectRatio = $derived(originalImageWidth / originalImageHeight);
+
+	let diceCountHorizontal = $state(100);
+	let diceCountVertical = $state(60);
+	let diceCountVerticalEffective = $derived(
+		lockOriginalImageAspectRatio
+			? Math.round(diceCountHorizontal / originalImageAspectRatio)
+			: diceCountVertical
+	);
+	let diceAspectRatio = $derived(diceCountHorizontal / diceCountVerticalEffective);
+
 	// $effect(() => {
 	//   const ctx = canvas.getContext('2d');
 
@@ -39,8 +53,12 @@
 				if (!canvas) {
 					throw new Error('Expected `canvas` to be initialized at this point.');
 				}
+
 				canvas.width = img.width;
 				canvas.height = img.height;
+
+				originalImageWidth = img.width;
+				originalImageHeight = img.height;
 
 				const ctx = canvas.getContext('2d');
 
@@ -70,5 +88,48 @@
 		<span>Upload your file</span>
 	</label>
 </p>
+
+<div>
+	<span>Original image aspect ratio:</span>
+	<span>{originalImageAspectRatio}</span>
+	<label>
+		<input type="checkbox" bind:checked={lockOriginalImageAspectRatio} />
+		Lock
+	</label>
+</div>
+
+<div>
+	<label>
+		<span>Dice count horizontal</span>
+		<input type="number" min="1" bind:value={diceCountHorizontal} />
+		<input type="range" min="1" max="200" bind:value={diceCountHorizontal} />
+	</label>
+</div>
+
+<div>
+	<label>
+		<span>Dice count vertical</span>
+		<input
+			type="number"
+			min="1"
+			value={diceCountVerticalEffective}
+			disabled={lockOriginalImageAspectRatio}
+			oninput={(e) => diceCountVertical = parseInt((e.target as HTMLInputElement).value, 10)}
+		/>
+		<input
+			type="range"
+			min="1"
+			max="200"
+			value={diceCountVerticalEffective}
+			disabled={lockOriginalImageAspectRatio}
+			oninput={(e) => diceCountVertical = parseInt((e.target as HTMLInputElement).value, 10)}
+		/>
+	</label>
+</div>
+
+<div>
+	<span>Dice aspect ratio</span>
+	<span>{diceAspectRatio}</span>
+</div>
 
 <canvas bind:this={canvas} width={size} height={size} style="width: 500px; height: auto"></canvas>
