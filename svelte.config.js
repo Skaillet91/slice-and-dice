d8a170b89ed53c7ace43413fba04f69932d23f35
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2024-06-30 21:50:40 +0300

    Revert to adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
@@ -11,7 +11,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '200.html',
+			// fallback: '200.html',
 			// may differ from host to host
 		}),
 		// https://panda-css.com/docs/installation/svelte
